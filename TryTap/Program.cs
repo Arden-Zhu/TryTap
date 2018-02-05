e2ffecf8f7be92a147e8de48a4f70355353c08b8
Arden Zhu <arden.zhu@lafayette148.com>
2018-02-05 15:12:28 -0500

    st 2

--- TryTap/Program.cs
+++ TryTap/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TryTap
@@ -12,26 +13,39 @@ namespace TryTap
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.UsageIoTapFunction();
-
-            program.TryCpuConsuming();
-
+            ShowThreadId("Main() 1");
+            program.UseIoTapFunction();
+            ShowThreadId("key to next step:Main() 2");
+            Console.ReadKey();
+            program.UseCpuTapFuntion();
+            ShowThreadId("key to next step:Main() 3");
             Console.ReadKey();
         }
 
-        private async void UsageIoTapFunction()
+        private static void ShowThreadId(string context)
         {
+            Console.WriteLine($"{context} in thread# {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private async void UseIoTapFunction()
+        {
+            ShowThreadId("UseIoTapFunction() 1");
             HttpClient client = new HttpClient();
             HttpResponseMessage httpResponse = 
                 await client.GetAsync(@"https://www.google.com/search?q=test");
+            ShowThreadId("UseIoTapFunction() 2");
             string content = await httpResponse.Content.ReadAsStringAsync();
+            ShowThreadId("UseIoTapFunction() 3");
             Console.WriteLine("content = " + content.Substring(0, 100));
         }
 
-        private async void TryCpuConsuming()
+        private async void UseCpuTapFuntion()
         {
-            int r = await FibAsync(6);
-            Console.WriteLine("FibAsync(6)=" + r.ToString());
+            ShowThreadId("UseCpuTapFuntion() 1");
+            int pos = 20;
+            int r = await FibAsync(pos);
+            ShowThreadId("UseCpuTapFuntion() 2");
+            Console.WriteLine($"FibAsync({pos})={r}");
         }
 
         private int Fib(int pos)
@@ -50,7 +64,14 @@ namespace TryTap
 
         private Task<int> FibAsync(int pos)
         {
-            return Task.Run<int>(() => Fib(pos));
+            ShowThreadId("FibAsync() 1");
+            Task<int> task =  Task.Run<int>(() =>
+            {
+                ShowThreadId($"Fib()");
+                return Fib(pos);
+            });
+            ShowThreadId("FibAsync() 2");
+            return task;
         }
     }
 }
